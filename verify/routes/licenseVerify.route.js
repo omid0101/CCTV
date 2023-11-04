4841b57a319c3314f53a69e18009a95c49359c9c
omid0101 <omid.doust2@gmail.com>
2023-11-04 14:50:30 -0400

    updates

--- verify/routes/licenseVerify.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const CLicenseVerify = require("../controllers/licenseVerify.controller");
-
-const licenseVerifyRouter = express.Router();
-
-licenseVerifyRouter.post("/license_verify", CLicenseVerify);
-
-module.exports = licenseVerifyRouter;
